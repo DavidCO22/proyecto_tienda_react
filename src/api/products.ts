8d250dd93a1e809d3311b47c41af9e937af7a1ec
David <davidcorreaworks@gmail.com>
2025-07-31 15:18:25 -0500

    se cambiaron las url de la api

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,11 +1,11 @@
 export async function getCategories() {
-    const response = await fetch('http://localhost:4000/api/categories');
+    const response = await fetch('https://apitiendamax-production.up.railway.app/api/categories');
     if (!response.ok) throw new Error('Network response was not ok');
     return await response.json();
 }
 
 export async function getProductsFilter(category:String,page:Number, limit:Number) {
-    const response = await fetch(`http://localhost:4000/api/productFilter?category=${category}&page=${page}&limit=${limit}`);
+    const response = await fetch(`https://apitiendamax-production.up.railway.app/api/productFilter?category=${category}&page=${page}&limit=${limit}`);
     if (!response.ok) throw new Error('Network response was not ok');
     return await response.json();
 }
